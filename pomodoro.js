936b3ebbd4f712b84ff98bb3914a7673b8fe8f1a
suppixie <mamthapatalay6@gmail.com>
2023-03-10 02:39:56 +0530

    Added Sounds

--- pomodoro.js
+++ pomodoro.js
@@ -12,17 +12,6 @@ let countdown;
 let seconds = 25 * 60; // 25 minutes
 let timerType = 'pomodoro';
 let pomodoroCount = 0;
-let initial=true;
-let pomoTime=25;
-let sBreak=5;
-let lBreak=15;
-
-function setTime(){
-  pomoTime= document.getElementById("workinput").value;
-  sBreak = document.getElementById("sbreakinput").value;
-  lBreak = document.getElementById("lbreakinput").value;
-  resetTimer();
-}
 
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
@@ -31,24 +20,21 @@ function displayTimeLeft(seconds) {
   timerDisplay.textContent = display;
   // document.title = display;
 }
-function setTimer(){
+
+function startTimer() {
   clearInterval(countdown);
   if (timerType === 'pomodoro') {
-    seconds = pomoTime * 60;
+    seconds = 25 * 60;
     pomodoroCount++;
     if (pomodoroCount % 4 === 0) {
-      seconds = lBreak * 60; // long break every 4th pomodoro
+      seconds = 15 * 60; // long break every 4th pomodoro
     }
   } else if (timerType === 'short-break') {
-    seconds = sBreak * 60;
+    seconds = 5 * 60;
   } else if (timerType === 'long-break') {
-    seconds = lBreak * 60;
+    seconds = 15 * 60;
   }
-  startTimer(seconds)
-}
-
-function startTimer(seconds) {
-    displayTimeLeft(seconds);
+  displayTimeLeft(seconds);
   countdown = setInterval(() => {
     seconds--;
     displayTimeLeft(seconds);
@@ -59,42 +45,23 @@ function startTimer(seconds) {
     }
   }, 1000);
 }
-function resumeTimer(){
-  clearInterval(countdown);
-  var currTime=timerDisplay.textContent.split(":");
-  var sec=(Number(currTime[0])*60)+(Number(currTime[1]));
-  startTimer(sec)
-}
-
-function toggleTimer(){
-  pauseButton.style.display="block";
-  startButton.style.display="none";
-  if(initial){
-    setTimer();
-  }else resumeTimer();
-}
 
 function pauseTimer() {
   clearInterval(countdown);
-  initial=false;
-  pauseButton.style.display="none";
-  startButton.style.display="block";
 }
 
-
 function resetTimer() {
-  initial=true;
   clearInterval(countdown);
   if (timerType === 'pomodoro') {
-    seconds = pomoTime * 60;
+    seconds = 25 * 60;
   }
   else if (timerType === 'short-break') {
-    seconds = sBreak * 60;
+    seconds = 5 * 60;
   }
   else if (timerType === 'long-break') {
-    seconds = lBreak * 60;
+    seconds = 15 * 60;
   }
-    displayTimeLeft(seconds);
+  displayTimeLeft(seconds);
 }
 
 function handleTimerTypeChange() {
@@ -105,7 +72,7 @@ function handleTimerTypeChange() {
 pomodoroButton.addEventListener('change', handleTimerTypeChange);
 shortBreakButton.addEventListener('change', handleTimerTypeChange);
 longBreakButton.addEventListener('change', handleTimerTypeChange);
-startButton.addEventListener('click', toggleTimer);
+startButton.addEventListener('click', startTimer);
 pauseButton.addEventListener('click', pauseTimer);
 resetButton.addEventListener('click', resetTimer);
 
@@ -115,31 +82,15 @@ function allowDrop(ev) {
   ev.preventDefault();
 }
 
-function drag(event) {
-  var style = window.getComputedStyle(event.target, null);
-    event.dataTransfer.setData("text/plain",
-    (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
-
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function drop(event) {
-  var offset = event.dataTransfer.getData("text/plain").split(',');
-  var dm = document.getElementById('dragme');
-  dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-  dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-  event.preventDefault();
-  return false;
-
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
 }
-function drag_over(event) { 
-  event.preventDefault(); 
-  return false; 
-} 
-
-var dm = document.getElementById('dragme'); 
-dm.addEventListener('dragstart',drag_start,false); 
-document.body.addEventListener('dragover',drag_over,false); 
-document.body.addEventListener('drop',drop,false); 
 
 // changing the alarms
 
@@ -167,23 +118,28 @@ selector.addEventListener('change', themes);
 
 // select Background sounds
 
-const ppBtn = document.getElementById("ppBtn").addEventListener("click",playPause());
-const tracks = document.getElementById("track");
-function playPause() { 
-  if (tracks.paused) {
-  tracks.play();
-  ppButton.innerHTML = "Pause";
-  }
-  else  {
-  tracks.pause(); 
-  ppButton.innerHTML = "Play";
-  }
-  } 
+// const playButtons = document.getElementsByClassName('audiobuttons');
+// const tracks = document.getElementsByClassName('audiofiles');
 
-  function thisVolume(volume_value){
-  var tracks = document.getElementById("track");
-  document.getElementById("vol").innerHTML=volume_value;
-  tracks.volume = volume_value / 100;}
+// for (let i = 0; i < playButtons.length; i++) {
+//   playButtons[i].addEventListener('click', function() {
+//     const track = tracks[i];
+//     const playButton = playButtons[i];
+    
+//     if (track.paused) {
+//       track.play();
+//       playButton.innerHTML = 'Pause'; // update the button text to 'Pause'
+//     } else {
+//       track.pause();
+//       playButton.innerHTML = 'Play'; // update the button text to 'Play'
+//     }
+    
+//     track.addEventListener('ended', () => {
+//       track.currentTime = 0; // reset the audio to the beginning
+//       playButton.innerHTML = 'Play'; // update the button text to 'Play'
+//     });
+//   });
+// }
 
 
 //add a task in the task list
@@ -192,50 +148,36 @@ function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
+  var hr = document.createElement("hr");
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
+    document.getElementById("myUL").appendChild(hr);
 
   }
   document.getElementById("myInput").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
-  li.className="taskItem";
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-  var list = document.getElementsByClassName("taskItem");
-  for (i = 0; i < list.length; i++) {
-    list[i].onclick=function (){this.classList.toggle('checked');}
-  }
+  var list = document.getElementById('myUL')
+  console.log(list)
+  list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+      ev.target.classList.toggle('checked');
+    }
+  }, false);
   const closeFunction = document.getElementsByClassName("close");
   var i;
   for (i = 0; i < closeFunction.length; i++) {
     closeFunction[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
-
     }
   }
 }
 
-
-
-// const audio = document.getElementsByClassName("track")
-// const playBtn = document.getElementById("play")
-
-// playBtn.addEventListener("click", function() {
-//   audio.play();
-// });
-
-// function play() {
-//   if (audio.paused){
-//   audio.play();}
-//   else {
-//     audio.pause()
-//   }
-  
-// }
